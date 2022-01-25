81e4291c7632684b7060d02b2a40c9be67d7f254
Furrukh Jamal <furrukhjamal@yahoo.com>
2022-01-26 00:28:53 +0500

    cleaned

--- src/App.js
+++ src/App.js
@@ -8,139 +8,13 @@ import TodosContext from './context/TodosContext';
 
 function App() {
   const nameInputEl = useRef(null)
-  //const[name, setName] = useState("")
   const [name, setName] = useLocalStorage("name", "") //key, intitialValue
   const [todos, setTodos] = useLocalStorage("todos", [])
   const [filter, setFilter] = useState("all") 
-  // const [todos, setTodos] = useState([
-  //   {
-  //     id : 1,
-  //     title : "Go to groceries",
-  //     iscompleted : false,
-  //     isEditing : false,
-
-  //   },
-  //   {
-  //     id : 2,
-  //     title : "Finish work",
-  //     iscompleted : true,
-  //     isEditing : false,
-
-  //   },
-  //   {
-  //     id : 3,
-  //     title : "Go to die",
-  //     iscompleted : false,
-  //     isEditing : false,
-
-  //   },])
-
-    const [allChecked, setAllChecked] = useState(false)
-    // const [inputTodo, setInputTodo] = useState("")
-    //const[idForTodo, setidForTodo] = useState(1)
-    const[idForTodo, setidForTodo] = useLocalStorage("idForTodo", 1)
+  const [allChecked, setAllChecked] = useState(false)
+  const[idForTodo, setidForTodo] = useLocalStorage("idForTodo", 1)
 
 
-    // function handleInput(e){
-    //   setInputTodo(e.target.value)
-    // }
-
-    // function addTodo(todo){
-    //   // e.preventDefault()
-    //   // if(inputTodo.trim().length === 0){
-    //   //   return;
-    //   // }
-    //   setTodos([...todos, {
-    //     id : idForTodo,
-    //     title: todo,
-    //     iscompleted : false
-    //   }])
-    //   //setInputTodo("")
-    //   setidForTodo(previous=>previous + 1)
-    // }
-
-    // function deleteTodo(id){
-    //   setTodos([...todos].filter(item=> item.id !== id))
-    // }
-
-    // function completeTodo(id){
-    //   const updatedTodos = todos.map(todo=> { 
-    //     if(todo.id === id)
-    //     {
-    //       todo.iscompleted = !todo.iscompleted
-    //     }
-    //     return todo
-    //   })
-    //   setTodos(updatedTodos)
-    // }
-
-
-    // function markAsEditing(id){
-    //   const updatedTodos = todos.map(todo=> { 
-    //     if(todo.id === id)
-    //     {
-    //       todo.isEditing = !todo.isEditing
-    //     }
-    //     return todo
-    //   })
-    //   setTodos(updatedTodos)
-    // }
-
-    // function updateTodo(event, id)
-    // {
-    //   const updatedTodos = todos.map(todo=> { 
-    //     if(todo.id === id)
-    //     {
-    //       if(event.target.value.trim().length === 0){
-    //         todo.isEditing = false
-    //         return todo;
-    //       }
-    //       todo.title = event.target.value
-    //       todo.isEditing = false 
-    //     }
-    //     return todo
-    //   })
-    //   setTodos(updatedTodos)
-    // }
-
-
-    // function cancelEdit(id){
-    //   const updatedTodos = todos.map(todo=> { 
-    //     if(todo.id === id)
-    //     {
-    //       todo.isEditing = false
-    //     }
-    //     return todo
-    //   })
-    //   setTodos(updatedTodos)
-    // }
-
-  // function remaining(){
-  //   return todos.filter(todo=> !todo.iscompleted).length
-  // }
-
-
-  // function remainingTest(){
-  //   //to display usage of useMemo
-  //   for(let i = 0; i < 2000000000; i++){}
-  //   return todos.filter(todo=> !todo.iscompleted).length
-  // }
-
-
-  // function clearCompleted(){
-  //   console.log("clear completed kicked in")
-  //   setTodos([...todos].filter(todo=> !todo.iscompleted))
-  // }
-
-
-  // function completeAllTodos(){
-  //   console.log("completeALLTodos clicked")
-  //   const updated = todos.map(todo => {
-  //     todo.iscompleted = true
-  //     return todo; 
-  //   })
-  //   setTodos(updated)
-  // }
 
 
   function todosFiltered()
@@ -199,19 +73,7 @@ function App() {
             
             {
               todos.length > 0 ? (
-                <TodoList 
-                  // todos = {todos}
-                  // completeTodo = {completeTodo}
-                  // updateTodo = {updateTodo}
-                  // cancelEdit = {cancelEdit}
-                  // markAsEditing = {markAsEditing}
-                  // deleteTodo = {deleteTodo}
-                  // allChecked = {allChecked}
-                  // remaining = {remaining} 
-                  //clearCompleted = {clearCompleted}
-                  //completeAllTodos = {completeAllTodos}
-                  todosFiltered = {todosFiltered}
-                  />
+                <TodoList todosFiltered = {todosFiltered}/>
               ) : (
                   <div>
                     <p>Add a Todo</p>
